eb59beeb600628e1eb15c21fd89a4541b4099677
ALAGALA YADHU VAMSHI <95372538+YadhuVamshi@users.noreply.github.com>
2026-06-15 00:07:21 +0530

    Delete multipledocs.py

--- multipledocs.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
-
-# Automatically detects and loads multiple documents
-loader = DirectoryLoader("documents", glob="**/*.pdf", loader_cls = PyPDFLoader)
-docs = loader.load()
-
-print(f"Loaded {len(docs)} documents for LLM processing.")
-print(docs[-1].page_content[:200])
